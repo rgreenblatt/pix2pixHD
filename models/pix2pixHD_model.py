a4e0fd23710676fe201916a29c5aa04fa3273d9d
rgreenblatt <greenblattryan@gmail.com>
2019-05-06 12:13:27 -0400

    Fixed inference

--- models/pix2pixHD_model.py
+++ models/pix2pixHD_model.py
@@ -260,7 +260,6 @@ class Pix2PixHDModel(BaseModel):
         if not self.opt.no_temporal_smoothing:
             input_concat = torch.cat((input_concat, prev_fake), dim=1)
 
-
         if torch.__version__.startswith('0.4'):
             with torch.no_grad():
                 fake_image = self.netG.forward(input_concat)
